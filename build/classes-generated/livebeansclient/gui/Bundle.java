ea2b2c6c1d884f81998c657fb5211934c816c906
Danielnaor <dnaorcode@gmail.com>
2023-04-11 12:18:25 -0500

    first commit

---
+++ build/classes-generated/livebeansclient/gui/Bundle.java
@@ -0,0 +1,12 @@
+package livebeansclient.gui;
+/** Localizable strings for {@link livebeansclient.gui}. */
+class Bundle {
+    /**
+     * @return <i>Connect to LiveBeans server</i>
+     * @see LiveBeansToolbarAction
+     */
+    static String CTL_LiveBeansToolbarAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_LiveBeansToolbarAction");
+    }
+    private Bundle() {}
+}
